727682788f4966ffbb13f14d540af9cbdbba8081
KimJaeGeun <jgk9612@gmail.com>
2022-10-28 15:13:14 +0900

    refactor: Fix User to use lombok

--- src/main/java/com/api/springbootapi/domain/User.java
+++ src/main/java/com/api/springbootapi/domain/User.java
@@ -1,14 +1,15 @@
 package com.api.springbootapi.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
     private String id;
     private String name;
     private String password;
-
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
 }
 
